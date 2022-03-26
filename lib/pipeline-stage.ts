94abe946219b08b69660cf392f3e1f37029bb056
James Meyers <james.meyers@emberex.com>
2022-03-25 21:07:15 -0700

    Fix bad id passing.

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -6,6 +6,6 @@ export class WorkshopPipelineStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
-    new CdkWorkshopStack(scope, id, props);
+    new CdkWorkshopStack(scope, "WebService");
   }
 }
