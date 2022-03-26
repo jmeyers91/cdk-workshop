4cda17389d56b2d19af1829b3f32ebce21de4020
James Meyers <james.meyers@emberex.com>
2022-03-25 20:21:19 -0700

    Add build step to the pipeline.

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,13 +1,27 @@
 import * as cdk from "aws-cdk-lib";
 import * as codecommit from "aws-cdk-lib/aws-codecommit";
+import {
+  CodeBuildStep,
+  CodePipeline,
+  CodePipelineSource,
+} from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class WorkshopPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new codecommit.Repository(this, "WorkshopRepo", {
+    const codecommitRepo = new codecommit.Repository(this, "WorkshopRepo", {
       repositoryName: "WorkshopRepo",
+    });
+
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "WorkshopPipeline",
+      synth: new CodeBuildStep("SynthStep", {
+        input: CodePipelineSource.codeCommit(codecommitRepo, "main"),
+        installCommands: [""],
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
     });
   }
 }
